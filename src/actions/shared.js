46dc51274389c13dc6ae5902823ca4b3808b1ccd
Taryn Sauer <taryn.sauer@gmail.com>
2018-08-13 16:21:59 -0500

    chore: add authedUser placeholders

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,13 +1,17 @@
 import { getInitialData } from '../utils/api'
+import { setAuthedUser } from './authedUser'
 import { receiveQuestions } from './questions'
 import { receiveUsers } from './users'
+
+const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData () {
   return (dispatch) => {
     return getInitialData()
       .then(({ users, questions }) => {
-        dispatch(receiveUsers(users))
+        dispatch(setAuthedUser(AUTHED_ID))
         dispatch(receiveQuestions(questions))
+        dispatch(receiveUsers(users))
       })
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
-import users from './users'
+import authedUser from './authedUser'
 import questions from './questions'
+import users from './users'
 
 export default combineReducers({
-  users,
+  authedUser,
   questions,
+  users,
 })
