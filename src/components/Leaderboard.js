7842df70cb02dbaf2dc9808da3df4517e837acb1
Taryn Sauer <taryn.sauer@gmail.com>
2018-08-19 21:02:07 -0500

    feat: Add Leaderboard component guts

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -4,15 +4,46 @@ import { connect } from 'react-redux'
 class Leaderboard extends Component {
   render() {
     return (
-      <ul>
-        Leaderboard
-      </ul>
+      <div>
+        <h1>Leaderboard</h1>
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>User</th>
+              <th>Asked Count</th>
+              <th>Answered Count</th>
+            </tr>
+            {this.props.userStats.map((stat) =>
+              <tr key={stat.id}>
+                <td>{stat.avatarURL}</td>
+                <td>{stat.name}</td>
+                <td>{stat.questionsCount}</td>
+                <td>{stat.answersCount}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
 
-function mapStateToProps ({ authedUser, questions, users }) {
+function mapStateToProps ({ users }) {
   return {
+    userStats: Object.values(users).map((user) => {
+      const questionsCount = user.questions.length
+      const answersCount = Object.values(user.answers).length
+
+      return {
+        id: user.id,
+        name: user.name,
+        avatarURL: user.avatarURL,
+        questionsCount: questionsCount,
+        answersCount: answersCount,
+        total: questionsCount + answersCount,
+      }
+    }).sort((a, b) => a.total < b.total),
   }
 }
 
