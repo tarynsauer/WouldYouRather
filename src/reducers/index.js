57aa4eb0bdffc14dee0809c637a5493a2834929b
Taryn Sauer <taryn.sauer@gmail.com>
2018-08-22 09:24:53 -0500

    wip - loading bar not working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import { loadingBarReducer } from 'react-redux-loading'
 import authedUser from './authedUser'
 import questions from './questions'
 import users from './users'
@@ -7,4 +8,5 @@ export default combineReducers({
   authedUser,
   questions,
   users,
+  loadingBar: loadingBarReducer,
 })
