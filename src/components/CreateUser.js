715afe88ef9b21d94eddc2c2a77085c7e3c201ea
Taryn Sauer <taryn.sauer@gmail.com>
2018-08-26 20:45:14 -0500

    feat: Add additional input validation to user create

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -27,7 +27,7 @@ class CreateUser extends Component {
     const { usersList } = this.props
     const result = usersList.filter(user => (user.id === this.state.username))
     if (result.length > 0) {
-      errors.push(`Username ${username} already exists.`)
+      errors.push(`Username ${username} already exists`)
     }
 
     const inputs = Object.entries({ username, password, name })
@@ -36,6 +36,14 @@ class CreateUser extends Component {
 
     if (invalidInputNames.length > 0) {
       errors.push(`Missing required inputs: ${invalidInputNames.join(', ')}`)
+    }
+
+    if (username.length > 0 && username.length < 3) {
+      errors.push('Username must be at least three characters long')
+    }
+
+    if (password.length > 0 && password.length < 9) {
+      errors.push('Password must be at least nine characters long')
     }
 
     return errors
@@ -70,20 +78,18 @@ class CreateUser extends Component {
 
   render() {
     return (
-      <div>
-        <div className='create-user-container'>
-          <h2>Create user</h2>
-          <div className='errors'>{this.state.errors.join(', ')}</div>
-          <form onSubmit={this.handleSubmit}>
-            <div className='login-inputs'>
-              <input type='text' onChange={this.handleChange} value={this.state.name} name='name' placeholder='Name' autoComplete='name' />
-              <input type='text' onChange={this.handleChange} value={this.state.username} name='username' placeholder='Username' autoComplete='username' />
-              <input type='text' onChange={this.handleChange} value={this.state.avatarURL} name='avatarURL' placeholder='Avatar URL' autoComplete='avatar-url' />
-              <input type='password' onChange={this.handleChange} value={this.state.password} name='password' placeholder='Password' autoComplete='current-password' />
-            </div>
-            <input type='submit' value='Create User' />
-          </form>
-        </div>
+      <div className='login-container'>
+        <h2>Create user</h2>
+        <div className='errors'>{this.state.errors.join(', ')}</div>
+        <form onSubmit={this.handleSubmit}>
+          <div className='login-inputs'>
+            <input type='text' onChange={this.handleChange} value={this.state.name} name='name' placeholder='Name' autoComplete='name' />
+            <input type='text' onChange={this.handleChange} value={this.state.username} name='username' placeholder='Username' autoComplete='username' />
+            <input type='text' onChange={this.handleChange} value={this.state.avatarURL} name='avatarURL' placeholder='Avatar URL' autoComplete='avatar-url' />
+            <input type='password' onChange={this.handleChange} value={this.state.password} name='password' placeholder='Password' autoComplete='current-password' />
+          </div>
+          <input type='submit' value='Create User' />
+        </form>
       </div>
     )
   }
