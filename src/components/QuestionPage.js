6f7502f6ee0a3d2d13b2007abd1fd04706de96ce
Taryn Sauer <taryn.sauer@gmail.com>
2018-08-31 20:06:47 -0500

    fix: redirect invalid question id to 404 page

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleAnswerQuestion } from '../actions/shared'
 import PropTypes from 'prop-types'
+import { Redirect } from 'react-router-dom'
 import AnsweredQuestion from './AnsweredQuestion'
 import UnansweredQuestion from './UnansweredQuestion'
 
@@ -18,7 +19,12 @@ class QuestionPage extends Component {
   }
 
   render() {
-    const { question, author, user } = this.props
+    const { question, author, user, invalid } = this.props
+
+    if (invalid) {
+      return <Redirect to='/404' />
+    }
+
     const { optionOne, optionTwo } = question
     const unansweredByUser = !Object.keys(user.answers).includes(question.id)
     const userAnswer = user.answers[question.id]
@@ -42,17 +48,22 @@ class QuestionPage extends Component {
 function mapStateToProps ({ authedUser, questions, users }, props) {
   const question = questions[props.match.params.id]
 
+  if (question === undefined) {
+    return { invalid: true }
+  }
+
   return {
     author: users[question.author],
     question: question,
     user: users[authedUser],
+    invalid: false,
   }
 }
 
 QuestionPage.propTypes = {
-  author: PropTypes.object.isRequired,
-  question: PropTypes.object.isRequired,
-  user: PropTypes.object.isRequired,
+  author: PropTypes.object,
+  question: PropTypes.object,
+  user: PropTypes.object,
 }
 
 export default connect(mapStateToProps)(QuestionPage)
